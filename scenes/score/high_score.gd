71ad19d81d2e32f7df81b5dbf749fc848628d6a3
fizzydrinks <gabriel@lins.dev>
2023-11-15 11:51:24 -0300

    Show zero score by default

--- scenes/score/high_score.gd
+++ scenes/score/high_score.gd
@@ -1,5 +1,9 @@
 extends Label
 
 
+func _ready():
+	set_text('0')
+
+
 func _on_level_high_score_updated(score: int):
 	set_text(str(score))
--- scenes/score/score.gd
+++ scenes/score/score.gd
@@ -4,5 +4,9 @@ extends Label
 @export var prefix: String = ''
 
 
+func _ready():
+	set_text(prefix + '0')
+
+
 func _on_level_score_changed(score: int):
 	set_text(prefix + str(score))
