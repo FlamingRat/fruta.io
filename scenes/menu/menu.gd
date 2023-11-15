ead3acafd45c471374f6a1f3f3987d9aaacf6e8a
fizzydrinks <gabriel@lins.dev>
2023-11-15 01:40:52 -0300

    Reinstall android build template

--- scenes/menu/menu.gd
+++ scenes/menu/menu.gd
@@ -1,5 +1,25 @@
 extends Control
 
 
+var play_games_services
+
+
+func _ready():
+	if Engine.has_singleton("GodotPlayGamesServices"):
+		play_games_services = Engine.get_singleton("GodotPlayGamesServices")
+
+		play_games_services.init(true, false, false, "")
+		
+		play_games_services.connect(
+			"_on_sign_in_success", _on_sign_in_success
+		)
+		
+		var error = play_games_services.signIn()
+
+
+func _on_sign_in_success(acc_data):
+	print(acc_data)
+
+
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/level/level.tscn")
