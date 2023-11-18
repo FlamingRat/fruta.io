113cc40457fc4ccbe84e638d5a009e247b75bced
fizzydrinks <gabriel@lins.dev>
2023-11-18 14:15:47 -0300

    Add spin to combo stars

--- scenes/level/combo_star.gd
+++ scenes/level/combo_star.gd
@@ -12,11 +12,13 @@ var force_bonus: int = 0
 
 func _ready():
 	var upward_force = (300 + (100 * force_bonus)) * randf_range(0.8, 1.2)
+	var xdir = randi_range(0, 1) * 2 - 1
 	apply_impulse(
 		Vector2.UP * upward_force + randf_range(-0.5, 0.5) * 
 		Vector2.LEFT * 300,
-		Vector2.LEFT * 0.5 * (randi_range(0, 1) * 2 - 1)
+		Vector2.LEFT * 0.5 * xdir,
 	)
+	angular_velocity = xdir * randi_range(1, 3)
 
 
 func _process(delta):
